79fa3157666485f39f8b67b81b0cd4f441ee2fd8
Jesmin Chakma <jesminchakma39@gmail.com>
2025-04-25 12:07:57 +0600

    load more button expand added

--- js/script.js
+++ js/script.js
@@ -16,9 +16,46 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     })
 })
+
+
+/*  This is for to go to top after arrow_icon clicked  */
+
 document.querySelector('.copy_right .arrow-icon').addEventListener('click', function() {
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
 });
+
+
+/* This is for expanded Testimonials*/
+
+let isExpanded = false;
+
+function isMobile() {
+    return window.innerWidth <= 768;
+}
+
+function handleTestimonials() {
+    const testimonials = document.querySelectorAll('.testimonials');
+    const loadMoreButton = document.querySelector('.loadmore_button');
+    
+    if (!isExpanded && isMobile()) {
+        testimonials.forEach((testimonial, index) => {
+            testimonial.classList.toggle('hidden', index >= 4);
+        });
+        loadMoreButton.classList.remove('hidden');
+    } else {
+        testimonials.forEach(testimonial => testimonial.classList.remove('hidden'));
+        loadMoreButton.classList.add('hidden');
+    }
+}
+
+window.addEventListener('load', handleTestimonials);
+window.addEventListener('resize', handleTestimonials);
+
+document.querySelector('.loadmore_button').addEventListener('click', () => {
+    isExpanded = true;
+    handleTestimonials();
+});
+
